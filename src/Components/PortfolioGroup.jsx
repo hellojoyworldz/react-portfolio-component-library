54a0987e32666aa36fcbd42bb13ca44586f61c41
hellojoyworldz <hellojoyworldz@gmail.com>
2024-06-20 19:02:47 +0900

    refactor: 폴더명 변경 - Components -> components

--- src/Components/PortfolioGroup.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "../assets/styles/reset.scss";
-import "../assets/styles/common.scss";
-
-export const PortfolioGroup = ({ children }) => {
-  return <>{children}</>;
-};
